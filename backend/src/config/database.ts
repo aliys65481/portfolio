b402a2d734880076abd447a4d95a60cd8bbcf6ea
Mrx <MRX@email.com>
2026-09-25 13:14:28 +0330

    feat(db connection)

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-async function connectDatabase() {
-    const mongoConnectionUrl = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}?authSource=${process.env.MONGODB_AUTH_SOURCE}`
-    
-    await mongoose.connect(mongoConnectionUrl);
-}
-export {connectDatabase};
 
-// mongodb://admin:admin@localhost:27017/?authSource=admin
+async function connectDatabase() {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) throw new Error("MONGODB_URI is missing");
+
+  await mongoose.connect(uri);
+}
+
+export { connectDatabase };
